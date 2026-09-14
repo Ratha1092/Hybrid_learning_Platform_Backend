d0daadc8d03c218c76499bf44cbd33cbb5ec2dcd
Ratha1092 <www.RathaKH1092@gmail.com>
2026-09-14 15:12:17 +0700

    add

--- resources/views/livewire/notification-bell.blade.php
+++ resources/views/livewire/notification-bell.blade.php
@@ -256,6 +256,7 @@ html:not(.dark) {
 
 <div
     class="nb-wrap"
+    wire:poll.15s="refreshUnreadCount"
     x-data="{
         open: false,
         unread: $wire.entangle('unreadCount'),
@@ -288,7 +289,12 @@ html:not(.dark) {
         <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.8" aria-hidden="true">
             <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"/>
         </svg>
-        <span class="nb-badge" x-show="unread > 0" x-text="unread > 99 ? '99+' : unread"></span>
+        <span
+            class="nb-badge"
+            x-show="unread > 0"
+            x-text="unread > 99 ? '99+' : unread"
+            @if($unreadCount <= 0) style="display:none" @endif
+        >{{ $unreadCount > 99 ? '99+' : $unreadCount }}</span>
     </button>
 
     {{-- Dropdown panel --}}
